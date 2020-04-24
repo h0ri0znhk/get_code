6be7eeb3d2daf646dc8d6cf51d38cf282f2972f5
Randy Yeung <h0riz0n.yeung@gmail.com>
2020-04-23 23:28:22 -0500

    experimenting with different download methods

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+/*
+
+About: Gets random code from internet; mainly github and gitlabs, but should wotk with standard HTML
+*/
+
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    let link = "https://raw.githubusercontent.com/BenRoe/MMM-SystemStats/master/MMM-SystemStats.js";
+
+    let resp = reqwest::get(link).await?.text().await?;
+    println!("{:#?}", resp);
+    Ok(())
 }
